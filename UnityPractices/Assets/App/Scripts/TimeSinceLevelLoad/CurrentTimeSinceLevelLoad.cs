31efc8a0413f3e1c00530185cfbac389b674a950
eyedropsP <eyedropsp@gmail.com>
2021-02-19 02:33:36 +0900

    TimeSinceLevelLoadのサンプル追加

---
+++ UnityPractices/Assets/App/Scripts/TimeSinceLevelLoad/CurrentTimeSinceLevelLoad.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace App.Scripts.TimeSinceLevelLoad
+{
+	public class CurrentTimeSinceLevelLoad : MonoBehaviour
+	{
+		public Button Button;
+		public Text TimeText;
+
+		private void Awake()
+		{
+			Button.onClick.AddListener(CurrentTime);
+			TimeText.text = "Click Button";
+		}
+
+		private void CurrentTime()
+		{
+			TimeText.text = $"TimeSinceLevelLoad : {Time.timeSinceLevelLoad}";
+		}
+	}
+}
---
+++ UnityPractices/Assets/App/Scripts/TimeSinceLevelLoad/TimeSinceLevelLoad.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace App.Scripts.TimeSinceLevelLoad
+{
+	public class TimeSinceLevelLoad : MonoBehaviour
+	{
+		public Button button;
+		public Text text;
+
+		private void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+			
+			DontDestroyOnLoad(GameObject.Find("Canvas"));
+
+			if (button == null) return;
+			button.onClick.AddListener(LoadSceneButton);
+		}
+
+		private void Update()
+		{
+			text.text = $"Time Since Loaded : {Time.timeSinceLevelLoad:0000}";
+		}
+
+		private static void LoadSceneButton()
+		{
+			SceneManager.LoadSceneAsync("Scene2");
+		}
+	}
+}
